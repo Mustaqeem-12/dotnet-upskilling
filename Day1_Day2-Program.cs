ada6ee8446fc3c05297e8369d50124708f37d2ae
Mustaqeem-12 <mustaqeemmohammed37332@gmail.com>
2026-02-27 16:24:25 +0530

    day1 & day2 : .Net CLI and C# basic

---
+++ Day1_Day2-Program.cs
@@ -0,0 +1,11 @@
+     //DATA TYPES
+int RollNo = 101;
+string Name = "mustaqeem";
+double Marks = 97.5;
+bool Ispassed = true;
+Console.WriteLine($"Roll No:{RollNo}, Name:{Name}, Marks:{Marks}, Passed:{Ispassed}");
+
+
+     //OPERATORS
+int Num1 = 10, Num2 = 5;
+Console.WriteLine($"Addition:{Num1+Num2}, Subtraction:{Num1-Num2}, Multiplication:{Num1*Num2}, Division:{Num1/Num2}");   
