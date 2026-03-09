6f305395fd28152423845ad2e78c5fddcc017cde
Mustaqeem-12 <mustaqeemmohammed37332@gmail.com>
2026-03-09 16:24:46 +0530

    Add StudentController with Info action method

---
+++ Day10-Routing-Controller/StudentController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _2ndweekasp.net.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Info()
+        {
+            return View();
+        }
+    }
+}
