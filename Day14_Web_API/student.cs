a4309b5499a1a25c0e3cd9938dc6577ae901fff2
Mustaqeem-12 <mustaqeemmohammed37332@gmail.com>
2026-03-14 18:12:23 +0530

    Create student.cs

---
+++ Day14_Web_API/student.cs
@@ -0,0 +1,11 @@
+namespace Day14WebAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Course { get; set; }
+    }
+}
