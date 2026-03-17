d41504141cbdc1ca37e6a7ee7e8e63c659c530d6
Mustaqeem-12 <mustaqeemmohammed37332@gmail.com>
2026-03-17 15:00:46 +0530

    Create model.cs

---
+++ Day15&16_ Entity Framework Core /model.cs
@@ -0,0 +1,10 @@
+namespace InventoryApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? ProductName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
