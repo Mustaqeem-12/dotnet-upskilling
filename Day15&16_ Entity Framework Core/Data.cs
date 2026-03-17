f06012372d2b25645d12c2717b5c8606e78c9c00
Mustaqeem-12 <mustaqeemmohammed37332@gmail.com>
2026-03-17 15:01:58 +0530

    Create Data.cs

---
+++ Day15&16_ Entity Framework Core/Data.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using InventoryApi.Models;
+
+namespace InventoryApi.Data
+{
+    public class InventoryDbContext : DbContext
+    {
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
