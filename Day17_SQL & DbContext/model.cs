7ab3087d051746b8d341c27c169b9ff7b2c2f13a
Mustaqeem-12 <mustaqeemmohammed37332@gmail.com>
2026-03-18 18:13:54 +0530

    Create model.cs

---
+++ Day17_SQL & DbContext/model.cs
@@ -0,0 +1,11 @@
+namespace JewelleryApp.Models
+{
+    public class jewel
+    {
+        public int Id { get; set; }
+        public string JewelleryName { get; set; }
+        public string Type { get; set; }
+        public string Weight { get; set; }
+        public int Price { get; set; } 
+    }
+}
