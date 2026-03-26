75eab631f28e665b674ab96ace1f3681d387ba25
Mustaqeem-12 <mustaqeemmohammed37332@gmail.com>
2026-03-26 23:33:05 +0530

    Create AuthController.cs

---
+++ Day22_Authentication & Authorization (JWT Basics)/AuthController.cs
@@ -0,0 +1,53 @@
+using LuLuMall.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IConfiguration _config;
+
+    public AuthController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody]LoginModel model)
+    {
+        if (_config["Jwt:Key"] == null)
+        {
+            return StatusCode(500, "Jwt Key is Null");
+        }
+        if (model.Username == "lulumall" && model.Password == "lulumall1234")
+        {
+            var token = GenerateToken(model.Username);
+            return Ok(new { token });
+        }
+        return Unauthorized();
+    }
+
+    private string GenerateToken(string username)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Name, username)
+        };
+        var token = new JwtSecurityToken(
+            issuer: _config["Jwt:Issuer"],
+            audience: _config["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: creds
+            );
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
