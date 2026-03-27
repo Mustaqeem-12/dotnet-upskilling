752bf5a34a3d58ad2c080db46ef4bd67fabbfa5a
Mustaqeem-12 <mustaqeemmohammed37332@gmail.com>
2026-03-27 18:34:29 +0530

    Create Service.cs

---
+++ Day23-LINQ (advanced queries)/Service.cs
@@ -0,0 +1,42 @@
+using LuLuMall.Models;
+using LuLuMall.Repositories;
+
+namespace LuLuMall.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductService(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+        public List<Product> GetProducts()
+        {
+            return _repo.GetAll();
+        }
+        public List<Product> GetProducts(string? search, int page, int pageSize,string? sort)
+        {
+            return _repo.GetFilteredProducts(search,page,pageSize,sort);
+        }
+        
+        public void AddProduct(Product product)
+        {
+            _repo.Add(product);
+        }
+        public Product GetProductById(int id)
+        {
+            return _repo.GetById(id);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _repo.Update(product);
+        }
+
+        public void DeleteProduct(int id)
+        {
+            _repo.Delete(id);
+        }
+    }
+}
