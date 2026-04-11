d85e41f90a213f6c1155a1f1424668a12e1a046f
Mustaqeem-12 <mustaqeemmohammed37332@gmail.com>
2026-04-11 13:05:24 +0530

    Add Department class for employee management

---
+++ Day28_EmployeeManagement/Department.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public List<Employee> Employees { get; set; } = new();
+
+    }
+}
+
