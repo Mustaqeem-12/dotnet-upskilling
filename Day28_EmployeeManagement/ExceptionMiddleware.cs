2fa014c457d2938d0b381c16dc6d76be3f11839c
Mustaqeem-12 <mustaqeemmohammed37332@gmail.com>
2026-04-11 13:04:48 +0530

    Add ExceptionMiddleware for global error handling

---
+++ Day28_EmployeeManagement/ExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using System.Net;
+using System.Text.Json;
+
+namespace EmployeeManagement.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch(Exception ex)
+            {
+                await HandleException(context, ex);
+            }
+        }
+        private static Task HandleException(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var result = JsonSerializer.Serialize(new
+            {
+
+                message = "Sometime went wrong!",
+                error = ex.Message
+            });
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
